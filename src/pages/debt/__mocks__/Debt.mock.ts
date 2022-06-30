e3abd98da4803187b8403b7541fd1e715d34018b
Emerson Vieira <33058881+emevieira123@users.noreply.github.com>
2022-06-30 11:56:15 -0400

    Create arquivo de mock

---
+++ src/pages/debt/__mocks__/Debt.mock.ts
@@ -0,0 +1,32 @@
+export const DebtMock = [
+  {
+    debtId: '0354f2d6-a56c-4e67-8186-e39895c914df',
+    debtName: 'Avenida',
+    product: 'Compra de roupas',
+    valorParcela: 63,
+    valorTotal: 1000,
+    parcelasPagas: 1,
+    parcelasTotais: 3,
+    diaVencimento: 10,
+    parcelas: [
+      {
+        parcelaId: 'E8D68B34-4031-4C88-AABC-C417C993C554',
+        valorParcela: 63,
+        diaVencimento: 10,
+        status: true,
+      },
+      {
+        parcelaId: '40553638-F380-4063-9975-E8F4172E85FF',
+        valorParcela: 63,
+        diaVencimento: 10,
+        status: true,
+      },
+      {
+        parcelaId: 'E8D68B34-4131-4C88-AABC-C417C993C554',
+        valorParcela: 63,
+        diaVencimento: 10,
+        status: true,
+      },
+    ],
+  },
+];
